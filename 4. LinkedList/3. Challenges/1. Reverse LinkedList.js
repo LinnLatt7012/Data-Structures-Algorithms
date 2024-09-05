3619cbd8b32aafbfeef4d417eebefff24c4b5f5d
Linn Latt Oo <linnlattoo@gusto.edu.mm>
2024-09-05 02:57:02 +0000

    recursive reverse added

--- 4. LinkedList/3. Challenges/1. Reverse LinkedList.js
+++ 4. LinkedList/3. Challenges/1. Reverse LinkedList.js
@@ -24,7 +24,25 @@ class LinkedList {
     this.tail = newNode;
     this.length++;
   }
-
+  recur(node,prev){
+    if(!node){
+      this.head = prev;
+      return
+    }
+    let nextNode = node.next;
+    node.next=prev;
+    this.recur(nextNode,node)
+  }
+  reverse_recur(){
+    if(!this.head){
+      return this;
+    }
+    let temp = this.head;
+    this.recur(this.head,null);
+    this.tail=temp;
+    this.tail.next=null;
+    return this;
+  } 
   reverse() {
     let temp = this.head;
     this.head = this.tail;
@@ -47,4 +65,4 @@ const myLinkedList = new LinkedList(1);
 myLinkedList.push(2);
 myLinkedList.push(3);
 myLinkedList.push(4);
-console.log(myLinkedList.reverse());
+console.log(myLinkedList.reverse_recur());
